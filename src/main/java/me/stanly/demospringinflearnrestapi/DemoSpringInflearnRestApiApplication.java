79e525d9ef8aa5e8d1086bed7f06c24b330126f0
ParkChunSo <cnsth123@gmail.com>
2019-01-31 21:28:31 +0900

    Initial commit

---
+++ src/main/java/me/stanly/demospringinflearnrestapi/DemoSpringInflearnRestApiApplication.java
@@ -0,0 +1,14 @@
+package me.stanly.demospringinflearnrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringInflearnRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringInflearnRestApiApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/me/stanly/demospringinflearnrestapi/DemoSpringInflearnRestApiApplicationTests.java
@@ -0,0 +1,17 @@
+package me.stanly.demospringinflearnrestapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringInflearnRestApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
