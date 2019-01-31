25ff09e87cdb000d19b2c1abcf3fe70760bdcfaa
ParkChunSo <cnsth123@gmail.com>
2019-01-31 22:07:52 +0900

    Created Event Entity

---
+++ src/main/java/me/stanly/demospringinflearnrestapi/event/Event.java
@@ -0,0 +1,27 @@
+package me.stanly.demospringinflearnrestapi.event;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+
+@Builder @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @EqualsAndHashCode(of = "id")
+public class Event {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location; // (optional)
+    private int basePrice;
+    private int maxPrice;
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus;
+}
+
---
+++ src/main/java/me/stanly/demospringinflearnrestapi/event/EventStatus.java
@@ -0,0 +1,5 @@
+package me.stanly.demospringinflearnrestapi.event;
+
+public enum EventStatus {
+    DREAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
